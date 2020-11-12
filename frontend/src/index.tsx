ce4223a588c1624c0a083c750793a6801c738d7c
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-12 21:59:51 +0100

    Setup project

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import store from './store';
+import {Provider} from 'react-redux';
+
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
